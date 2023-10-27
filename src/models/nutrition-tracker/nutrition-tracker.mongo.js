d46f9ae0b3bd51b5adb96d62b76fbcf5e532a5c2
tahmid-saj <tahmid.saj@gmail.com>
2023-10-27 02:43:08 -0400

    mongo db for nutrition tracker

---
+++ src/models/nutrition-tracker/nutrition-tracker.mongo.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const nutritionTrackedDaysSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dateTracked: {
+    type: Date,
+    required: true,
+  },
+  calories: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  macronutrients: {
+    type: {
+      carbohydrates: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      protein: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+      fat: {
+        type: Number,
+        required: true,
+        default: 0,
+      }
+    },
+    default: {},
+    required: true,
+  },
+  micronutrients: {
+    type: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        amount: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+        unit: {
+          type: String,
+          required: true,
+        }
+      }
+    ],
+    default: [],
+    required: false,
+  }
+});
+
+const nutritionTrackedDaysSummarySchema = new mongoose.Schema({
+  averageDailyCaloriesConsumption: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  averageDailyCarbohydratesConsumption: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  averageDailyProteinConsumption: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  averageDailyFatConsumption: {
+    type: Number,
+    required: true,
+    default: 0,
+  }
+});
+
+const nutritionTrackedDaysDatabase = new mongoose.model("NutritionTrackedDays", nutritionTrackedDaysSchema);
+const nutritionTrackedDaysSummaryDatabase = new mongoose.model("NutritionTrackedDaysSummary", nutritionTrackedDaysSummarySchema);
+
+module.exports = {
+  nutritionTrackedDaysDatabase,
+  nutritionTrackedDaysSummaryDatabase,
+}
