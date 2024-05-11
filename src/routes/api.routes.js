1e536d8996513bf583f1a070fc238c0feaef1639
tahmid-saj <tahmid.saj@gmail.com>
2024-05-11 16:01:03 -0400

    calories burned route

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const { nutritionTrackerRouter } = require("./nutrition-tracker/nutrition-tracker.router");
+const { caloriesBurnedRouter } = require("./calories-burned/calories-burned.router")
 
 const api = express.Router();
 
 api.use("/nutrition-tracker", nutritionTrackerRouter);
+api.use("/calories-burned", caloriesBurnedRouter)
 
 module.exports = {
   api,
