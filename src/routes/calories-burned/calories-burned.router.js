4ee594407446c6ec7133277a57e1edc2619aedfa
tahmid-saj <tahmid.saj@gmail.com>
2024-05-11 16:21:56 -0400

    calories burned controller

--- src/routes/calories-burned/calories-burned.router.js
+++ src/routes/calories-burned/calories-burned.router.js
@@ -1,17 +1,22 @@
 const express = require("express")
+
+const { httpGetTrackedCaloriesBurned,
+  httpPostTrackedCaloriesBurned, httpDeleteTrackedCaloriesBurned,
+  httpPutTrackedCaloriesBurned
+} = require("./calories-burned.controller")
  
 const caloriesBurnedRouter = express.Router()
 
 // TODO: move to env variables
 // user sign in
-caloriesBurnedRouter.get("/tracked-calories-burned/:userid/:email", )
+caloriesBurnedRouter.get("/tracked-calories-burned/:userid/:email", httpGetTrackedCaloriesBurned)
 
 // calories burned operations
-caloriesBurnedRouter.post("/tracked-calories-burned/:userid/:email/add", )
-caloriesBurnedRouter.delete("/tracked-calories-burned/:userid/:email/remove", )
+caloriesBurnedRouter.post("/tracked-calories-burned/:userid/:email/add", httpPostTrackedCaloriesBurned)
+caloriesBurnedRouter.delete("/tracked-calories-burned/:userid/:email/remove", httpDeleteTrackedCaloriesBurned)
 
 // user sign out
-caloriesBurnedRouter.put("/tracked-calories-burned/:userid/:email/update", )
+caloriesBurnedRouter.put("/tracked-calories-burned/:userid/:email/update", httpPutTrackedCaloriesBurned)
 
 module.exports = {
   caloriesBurnedRouter
