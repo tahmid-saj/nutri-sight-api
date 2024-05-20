068aabcd8de5fc9b5194522f12a0ac3b6e567032
tahmid-saj <tahmid.saj@gmail.com>
2024-05-19 22:04:38 -0400

    vercel updates

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const { testRouter } = require("./test-route/test-route.router")
 const { chatbotRouter } = require("./chatbot/chatbot.router")
 const { nutritionTrackerRouter } = require("./nutrition-tracker/nutrition-tracker.router");
 const { caloriesBurnedRouter } = require("./calories-burned/calories-burned.router")
 
 const api = express.Router();
 
+api.use("/testroute", testRouter)
 api.use("/chatbot", chatbotRouter)
 api.use("/nutrition-tracker", nutritionTrackerRouter);
 api.use("/calories-burned", caloriesBurnedRouter)
