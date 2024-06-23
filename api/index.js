ce8a7a0724d013d7473fd3cffbdd60e1ebd6e273
tahmid-saj <tahmid.saj@gmail.com>
2024-06-23 01:11:29 -0400

    increasing function duration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const mongoose = require('mongoose');
 require("dotenv").config();
-const { mongoConnect } = require("./src/services/mongodb/mongodb.service");
-const { app } = require("./src/app");
+const { mongoConnect } = require("../src/services/mongodb/mongodb.service");
+const { app } = require("../src/app");
 const server = http.createServer(app);
 
 const PORT = process.env.PORT;
