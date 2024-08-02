8982089c73bb2f2004dceaa6e819db15775ec9cd
tahmid-saj <tahmid.saj@gmail.com>
2024-08-02 15:21:02 -0400

    adding helmet

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require("helmet")
 
 const { api } = require("./routes/api.routes");
 
@@ -11,6 +12,7 @@ const app = express();
 // TODO: move to env
 app.use(cors());
 app.use(morgan("combined"));
+app.use(helmet())
 app.use(express.json());
 app.use(bodyParser.text());
 app.use("/v1", api);
