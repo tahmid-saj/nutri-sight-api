f0acb96bdf50c97e074778abbc4dcfb8f58794b8
tahmid-saj <tahmid.saj@gmail.com>
2024-08-04 14:29:09 -0400

    graphql nutrition tracked days update

--- src/graphql/nutrition-tracker/nutrition-tracker.model.js
+++ src/graphql/nutrition-tracker/nutrition-tracker.model.js
@@ -1,5 +1,6 @@
-const { getNutritionTrackedDays, getNutritionTrackedDaysSummary
-
+const { getNutritionTrackedDays, getNutritionTrackedDaysSummary,
+  addNutritionTrackedDay, updateNutritionTrackedDay, removeNutritionTrackedDay,
+  updateNutritionTrackedDays, updateNutritionTrackedDaysSummary
 } = require("../../models/nutrition-tracker/nutrition-tracker.mongo.crud")
 
 async function nutritionTrackedDaysByUser(userId, email) {
@@ -12,7 +13,42 @@ async function nutritionTrackedDaysSummaryByUser(userId, email) {
   return nutritionTrackedDaysSummary.nutritionTrackedDaysSummary
 }
 
+async function createUserNutritionTrackedDay(userId, email, nutritionTrackedDay) {
+  console.log("Posting nutrition tracked day");
+  addNutritionTrackedDay(userId, email, nutritionTrackedDay);
+  return true
+}
+
+async function deleteUserNutritionTrackedDay(userId, email, nutritionTrackedDate) {
+  removeNutritionTrackedDay(userId, email, nutritionTrackedDate);
+  console.log("Deleting nutrition tracked day");
+  return true
+}
+
+async function updateUserNutritionTrackedDay(userId, email, originalNutritionTrackedDay, updatedNutritionTrackedDay) {
+  console.log("Updating nutrition tracked day");
+  updateNutritionTrackedDay(userId, email, originalNutritionTrackedDay, updatedNutritionTrackedDay);
+  return true
+}
+
+async function updateUserNutritionTrackedDays(userId, email, nutritionTrackedDays) {
+  updateNutritionTrackedDays(userId, email, nutritionTrackedDays);
+  console.log("Putting nutrition tracked days");
+  return true
+}
+
+async function updateUserNutritionTrackedDaysSummary(userId, email, nutritionTrackedDaysSummary) {
+  updateNutritionTrackedDaysSummary(userId, email, nutritionTrackedDaysSummary);
+  console.log("Putting nutrition tracked days summary");
+  return true
+}
+
 module.exports = {
   nutritionTrackedDaysByUser,
-  nutritionTrackedDaysSummaryByUser
+  nutritionTrackedDaysSummaryByUser,
+  createUserNutritionTrackedDay,
+  deleteUserNutritionTrackedDay,
+  updateUserNutritionTrackedDay,
+  updateUserNutritionTrackedDays,
+  updateUserNutritionTrackedDaysSummary
 }
