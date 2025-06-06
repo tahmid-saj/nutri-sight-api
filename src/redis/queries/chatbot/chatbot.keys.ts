61183d174e914133daf8bfa2d3db65f6df2deccd
tahmid-saj <tahmid.saj@gmail.com>
2025-06-05 22:20:19 -0400

    caching chatbot requests

---
+++ src/redis/queries/chatbot/chatbot.keys.ts
@@ -0,0 +1,5 @@
+
+export const chatbotSessionRequestKey = (sessionId: string) => `chatbot-session-req#${sessionId}`
+
+export const chatbotSessionResponseKey = (sessionId: string) => `chatbot-session-res#${sessionId}`
+
