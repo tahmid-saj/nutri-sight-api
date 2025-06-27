01c44d4cca76448f3ec7184ad604ebccbfe55cc5
tahmid-saj <tahmid.saj@gmail.com>
2025-06-26 21:29:19 -0400

    nutrient pred ecs task

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import morgan from "morgan";
@@ -6,15 +6,29 @@ import helmet from "helmet";
 
 import { api } from "./routes/api.routes.js";
 
-const app = express() as any
+const app = express();
 
-// middleware
-// TODO: move to env
-app.use(cors());
+// CORS middleware
+app.use(cors({
+  origin: "https://www.nutritiontracker.io",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.nutritiontracker.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.options("*", cors()); // For preflight
+
 app.use(morgan("combined"));
-app.use(helmet())
+app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.text());
 app.use("/v1", api);
 
-export { app }
+export { app };
