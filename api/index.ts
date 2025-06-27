f267d1d9dd9e1ccd640824124026b37d5f0c03ed
tahmid-saj <tahmid.saj@gmail.com>
2025-06-26 20:50:46 -0400

    nutrient pred ecs task

--- api/index.ts
+++ api/index.ts
@@ -1,39 +1,25 @@
-import http from 'http';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { app } from "../src/app.js";
 import { mongoConnect } from "../src/services/mongodb/mongodb.service.js";
-import { app } from "../src/app.js"
 import { redisConnect } from '../src/services/redis/redis.service.js';
 
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;  // Provide a default port if PORT is undefined
+import serverless from 'serverless-http';
 
-async function startServer(): Promise<void> {
-  console.log("App is starting...");
+let isConnected = false;
 
-  process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
-  });
-
-  process.on('unhandledRejection', (reason) => {
-    console.error('Unhandled Rejection:', reason);
-  });
-
-  try {
+async function bootstrap() {
+  if (!isConnected) {
     await mongoConnect();
     await redisConnect();
-
-    server.listen(PORT, () => {
-      console.log(`Listening on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error("Fatal startup error: ", err);
-    process.exit(1); // Optionally fail fast
+    isConnected = true;
   }
+}
+
+const handler = async (req: any, res: any) => {
+  await bootstrap();
+  return serverless(app)(req, res);
 };
 
-startServer();
-
-export default app
+export default handler;
